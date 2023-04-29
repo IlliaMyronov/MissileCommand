0c21744940e64f9534c0262ca8819a7e4c68a5ab
GUI-MESSENGER <130620733+GUI-MESSENGER@users.noreply.github.com>
2023-04-29 17:19:20 -0500

    WORKED ON THE MAIN MENU

---
+++ game/MissileCommand/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	[SerializeField] private AudioSource select;
+	[SerializeField] private AudioSource boop;
+
+
+
+
+
+
+	public void Boop()
+	{
+		boop.Play();
+	}
+
+	public void StartGame()
+	{
+		select.Play();
+		SceneManager.LoadScene("Level 001");
+	}
+
+	public void OpenOptions()
+	{
+		select.Play();
+	}
+
+
+
+
+
+
+}
